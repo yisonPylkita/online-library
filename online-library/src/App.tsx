ea83941849b5113baeebd332f6580624bebc6248
Wojciech Bartnik <yisonPylkita@gmail.com>
2022-08-03 22:08:18 +0200

    React Table with content based on user input

--- online-library/src/App.tsx
+++ online-library/src/App.tsx
@@ -1,9 +1,79 @@
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Table from './Table'
+import axios from 'axios'
+import React from 'react'
+
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const columns = useMemo(
+    () => [
+      {
+        // first group - TV Show
+        Header: "TV Show",
+        // First group columns
+        columns: [
+          {
+            Header: "Name",
+            accessor: "show.name"
+          },
+          {
+            Header: "Type",
+            accessor: "show.type"
+          }
+        ]
+      },
+      {
+        // Second group - Details
+        Header: "Details",
+        // Second group columns
+        columns: [
+          {
+            Header: "Language",
+            accessor: "show.language"
+          },
+          {
+            Header: "Genre(s)",
+            accessor: "show.genres"
+          },
+          {
+            Header: "Runtime",
+            accessor: "show.runtime"
+          },
+          {
+            Header: "Status",
+            accessor: "show.status"
+          }
+        ]
+      }
+    ],
+    []
+  );
+
+  const [data, setData] = useState([]);
+
+  function useInput(opts: any) {
+    const [value, setValue] = React.useState('');
+    const input = <input
+      value={value}
+      onChange={e => setValue(e.target.value)}
+      {...opts} />
+
+    return [value, input];
+  }
+
+  const [username, usernameInput] = useInput({ placeholder: 'Type to search for specific phrase' });
+
+  useEffect(() => {
+    (async () => {
+      const result = await axios(`https://api.tvmaze.com/search/shows?q=${username}`);
+      setData(result.data);
+    })();
+  }, [username]);
+
 
   return (
     <div className="App">
@@ -17,7 +87,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 10)}>
           count is {count}
         </button>
         <p>
@@ -27,6 +97,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      {usernameInput}
+      <Table columns={columns} data={data} />
     </div>
   )
 }
